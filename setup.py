be51e3cb96d345a088f0703e7ea7577dfb5f0726
Nazem Mahmud <nazem.piash10@gmail.com>
2021-09-02 06:24:36 +0600

    update setup py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import io
 import os
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, find_namespace_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -9,12 +9,12 @@ setup(
     name="currency-converter-with-rate",    # This is the name of the package
     version="1.0.0",                        # The initial release version
     author="Nazem Mahmud Piash",            # Full name of the author
-    author_email='nazem.piash10' '@' 'gmail.com',
+    author_email='nazem.piash10@gmail.com',
     url='https://github.com/NazemMahmud/currencyConverterWithRate',
     description="Currency conversion between any two or to multiple and currency rates based on some parameters",
     long_description=long_description,      # Long description read from the the readme file
     long_description_content_type="text/markdown",
-    packages=find_packages(exclude=['docs', 'tests']),    # List of all python modules to be installed
+    packages=['currency-convert'],    # List of all python modules to be installed
     classifiers=[
         'Intended Audience :: Developers',
         "Programming Language :: Python :: 3",
@@ -23,8 +23,5 @@ setup(
         'Topic :: Software Development :: Internationalization',
     ],                                      # Information to filter the project on PyPi website
     python_requires='>=3.5',                # Minimum version requirement of the package
-    install_requires=[                      # Install other dependencies if any
-        'requests',
-        'simplejson',
-    ],
+    install_requires=['requests','simplejson'],  # Install other dependencies if any
 )
